879f2d3b39bf0fb2698aa874d1060d9cd12628ae
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-08-19 23:28:35 -0500

    Created model files for the database

---
+++ app/Models/Doctors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Doctors extends Model
+{
+    protected $table = 'doctors';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'specialty',
+        'identification',
+        'gender',
+        'phone',
+        'email',
+    ];
+}
---
+++ app/Models/Patients.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patients extends Model
+{
+    protected $table = 'patients';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'identification',
+        'dob',
+        'gender',
+        'phone',
+        'email',
+    ];
+
+    
+}
