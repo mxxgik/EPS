7445a43c8586d8235b275d022faa1f62c0545f80
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-08-20 22:11:17 -0500

    Finished routers

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,21 @@ use Illuminate\Support\Facades\Route;
 
 // Routers for patients 
 Route::get("listPatients", [PatientsController::class,"index"]);
+Route::get("showPatient/{id}", [PatientsController::class,"show"]);
 Route::post("createPatient", [PatientsController::class,"store"]);
+Route::put("editPatient/{id}", [PatientsController::class,"update"]);
+Route::delete("deletePatient/{id}", [PatientsController::class,"destroy"]);
 
 //Routers for doctors
 Route::get("listDoctors", [DoctorsController::class,"index"]);
+Route::get("showDoctor/{id}", [DoctorsController::class,"show"]);
+Route::post("createDoctor", [DoctorsController::class,"store"]);
+Route::put("editDoctor/{id}", [DoctorsController::class,"update"]);
+Route::delete("deleteDoctor{id}", [DoctorsController::class,"destroy"]);
 
 //Routers for appointments
 Route::get("listAppointments", [AppointmentsController::class,"index"]);
+Route::get("showAppointments/{id}", [AppointmentsController::class,"show"]);
+Route::post("createAppointments", [AppointmentsController::class,"store"]);
+Route::put("editAppointments/{id}", [AppointmentsController::class,"update"]);
+Route::delete("deleteAppointments{id}", [AppointmentsController::class,"destroy"]);
