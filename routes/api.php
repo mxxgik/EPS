0f49876320c245d4dd59dfe47a91a05d7d2a68e7
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-08-20 22:23:56 -0500

    Fixed delete routes

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ Route::get("listDoctors", [DoctorsController::class,"index"]);
 Route::get("showDoctor/{id}", [DoctorsController::class,"show"]);
 Route::post("createDoctor", [DoctorsController::class,"store"]);
 Route::put("editDoctor/{id}", [DoctorsController::class,"update"]);
-Route::delete("deleteDoctor{id}", [DoctorsController::class,"destroy"]);
+Route::delete("deleteDoctor/{id}", [DoctorsController::class,"destroy"]);
 
 //Routers for appointments
 Route::get("listAppointments", [AppointmentsController::class,"index"]);
 Route::get("showAppointments/{id}", [AppointmentsController::class,"show"]);
 Route::post("createAppointments", [AppointmentsController::class,"store"]);
 Route::put("editAppointments/{id}", [AppointmentsController::class,"update"]);
-Route::delete("deleteAppointments{id}", [AppointmentsController::class,"destroy"]);
+Route::delete("deleteAppointments/{id}", [AppointmentsController::class,"destroy"]);
