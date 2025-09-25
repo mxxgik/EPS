5d037454fcd44ade6bb6b469adbf1b697313bdd8
mxxgik <andres.chaparro7@hotmail.com>
2025-09-24 23:22:09 -0500

    Changed config to only return json objects instead of web pages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $th) {
+            return $request->is('api/*') || $request->expectsJson();
+        });
     })->create();
